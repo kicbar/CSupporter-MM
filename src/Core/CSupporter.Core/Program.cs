41f539505aad3416eae0afbe5359fbd2f56a28db
kicbar <bartlomiejkicior@gmail.com>
2021-10-23 23:22:02 +0200

    add project template

---
+++ src/Core/CSupporter.Core/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSupporter.Core
+{
+    public class Program
+    {
+        public static Task Main(string[] args) 
+            => CreateHostBuilder(args).Build().RunAsync();
+        
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
