a29121e1a7bed86d4af89f560d79b26f9ddaf992
kicbar <bartlomiejkicior@gmail.com>
2021-10-24 22:34:13 +0200

    Add contractor entity

---
+++ src/Modules/CSupporter.Modules.Contractors/Entities/Contractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSupporter.Modules.Contractors.Entities
+{
+    public class Contractor
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyDetails { get; set; }
+        public string NIP { get; set; }
+        public DateTime InsertDate { get; set; } = DateTime.Now;
+        public DateTime UpdateDate { get; set; } = DateTime.Now;
+    }
+}
