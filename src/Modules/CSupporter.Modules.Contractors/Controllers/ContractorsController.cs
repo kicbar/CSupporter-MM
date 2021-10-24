9242dcab0ae9764ea018c39a4f8535f77036a586
kicbar <bartlomiejkicior@gmail.com>
2021-10-24 13:05:17 +0200

    add test controller and method in Contractors Module

---
+++ src/Modules/CSupporter.Modules.Contractors/Controllers/ContractorsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSupporter.Modules.Contractors.Controllers
+{
+    [Route(template:"contractors-module")]
+    public class ContractorsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Hello Contracors!";
+    }
+}
