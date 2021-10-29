a60a0ef50123431655aa3f9de5579f8d0c3e744f
kicbar <bartlomiejkicior@gmail.com>
2021-10-29 19:16:35 +0200

    add view project

---
+++ src/Modules/CSupporter.Modules.View/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSupporter.Modules.View.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
