5eb27da8caac3bb96c0563ca457071802328505d
kicbar <bartlomiejkicior@gmail.com>
2021-12-05 17:41:54 +0100

    implement interface for position service

--- src/Modules/CSupporter.Modules.Factures/Application/Services/PositionService.cs
+++ src/Modules/CSupporter.Modules.Factures/Application/Services/PositionService.cs
@@ -1,4 +1,5 @@
 ﻿using CSupporter.Modules.Factures.Domain.Interfaces;
+using CSupporter.Shared.Infrastructure.Models.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,19 @@ namespace CSupporter.Modules.Factures.Application.Services
 {
     public class PositionService : IPositionService
     {
+        public PositionDto AddPositionToFacture(PositionDto positionDto, int factureId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PositionDto> GetAllPositionsForFacture(int factureId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemovePositionFromFacture(int positionId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
