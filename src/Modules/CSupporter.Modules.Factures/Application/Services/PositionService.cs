9859c9726a96c6e42e6c871c5303bbfb3de5aba0
kicbar <bartlomiejkicior@gmail.com>
2021-12-05 17:56:31 +0100

    implement repository and service for position of facture

--- src/Modules/CSupporter.Modules.Factures/Application/Services/PositionService.cs
+++ src/Modules/CSupporter.Modules.Factures/Application/Services/PositionService.cs
@@ -1,28 +1,39 @@
-﻿using CSupporter.Modules.Factures.Domain.Interfaces;
+﻿using AutoMapper;
+using CSupporter.Modules.Factures.Domain.Entities;
+using CSupporter.Modules.Factures.Domain.Interfaces;
 using CSupporter.Shared.Infrastructure.Models.DTOs;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSupporter.Modules.Factures.Application.Services
 {
     public class PositionService : IPositionService
     {
-        public PositionDto AddPositionToFacture(PositionDto positionDto, int factureId)
+        private readonly IMapper _mapper;
+        private readonly IPositionRepository _positionRepository;
+
+        public PositionService(IPositionRepository positionRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _positionRepository = positionRepository;
         }
 
         public List<PositionDto> GetAllPositionsForFacture(int factureId)
         {
-            throw new NotImplementedException();
+            return _mapper.Map<List<PositionDto>>(_positionRepository.GetAllPositionsForFacture(factureId));
+        }
+
+        public PositionDto AddPositionToFacture(PositionDto positionDto, int factureId)
+        {
+            Position position = _mapper.Map<Position>(positionDto);
+            position.FactureId = factureId;
+            _positionRepository.AddPositionToFacture(position);
+
+            return positionDto;
         }
 
         public bool RemovePositionFromFacture(int positionId)
         {
-            throw new NotImplementedException();
+            return _positionRepository.RemovePositionFromFacture(positionId);
         }
     }
 }
--- src/Modules/CSupporter.Modules.Factures/Infrastructure/Repositories/PositionRepository.cs
+++ src/Modules/CSupporter.Modules.Factures/Infrastructure/Repositories/PositionRepository.cs
@@ -1,28 +1,69 @@
 ﻿using CSupporter.Modules.Factures.Domain.Entities;
 using CSupporter.Modules.Factures.Domain.Interfaces;
+using CSupporter.Modules.Factures.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSupporter.Modules.Factures.Infrastructure.Repositories
 {
     public class PositionRepository : IPositionRepository
     {
-        public Position AddPositionToFacture(Position position)
+        private readonly CSupporterDbContext _csupporterDbContext;
+
+        public PositionRepository(CSupporterDbContext csupporterDbContext)
         {
-            throw new NotImplementedException();
+            _csupporterDbContext = csupporterDbContext;
         }
 
         public List<Position> GetAllPositionsForFacture(int factureId)
         {
-            throw new NotImplementedException();
+            CalculateFactureValue(factureId);
+            return _csupporterDbContext.Positions.Where(position => position.FactureId == factureId).ToList();
+        }
+
+        public Position AddPositionToFacture(Position position)
+        {
+            _csupporterDbContext.Add(position);
+            CalculateFactureValue(position.FactureId);
+            _csupporterDbContext.SaveChanges();
+            return position;
         }
 
         public bool RemovePositionFromFacture(int positionId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Position position = _csupporterDbContext.Positions.Where(position => position.PositionId == positionId).FirstOrDefault();
+
+                if (position != null)
+                {
+                    _csupporterDbContext.Remove(position);
+                    CalculateFactureValue(position.FactureId);
+                    _csupporterDbContext.SaveChanges();
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void CalculateFactureValue(int factureId)
+        {
+            double factureValue = 0;
+            List<Position> positions = _csupporterDbContext.Positions.Where(position => position.FactureId == factureId).ToList();
+
+            foreach (Position position in positions)
+            {
+                factureValue += position.ProductPrice * position.ProductAmount;
+            }
+
+            Facture facture = _csupporterDbContext.Factures.Where(facture => facture.FactureId == factureId).FirstOrDefault();
+            facture.Value = factureValue;
+            _csupporterDbContext.SaveChanges();
         }
     }
 }
