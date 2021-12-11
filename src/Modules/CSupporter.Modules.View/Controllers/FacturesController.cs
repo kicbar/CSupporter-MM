5e42387bd0c5495335b1e65a0f172746fbd17c36
kicbar <bartlomiejkicior@gmail.com>
2021-12-11 14:34:09 +0100

    add template for all facture view

---
+++ src/Modules/CSupporter.Modules.View/Controllers/FacturesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSupporter.Modules.View.Controllers
+{
+    public class FacturesController : Controller
+    {
+        public async Task<IActionResult> FacturesIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Modules/CSupporter.Modules.View/Models/FacturesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSupporter.Modules.View.Models
+{
+    public class FacturesDto
+    {
+        public int FactureId { get; set; }
+        public string FactureNo { get; set; }
+        public string FactureType { get; set; }
+        public DateTime FactureDate { get; set; } = DateTime.Now;
+        public double Value { get; set; }
+        public string ContractorId { get; set; }
+    }
+}
